1930396c9b6348c8d5143e244f27d0b0f44104e3
Kolmogortsev Konstantin <mr.kolmogortsev@gmail.com>
2022-02-23 13:30:45 -0500

    fixed brace positions and removed hungarian notation

--- src/main/java/log/LogWindowSource.java
+++ src/main/java/log/LogWindowSource.java
@@ -14,76 +14,62 @@ import java.util.Collections;
  */
 public class LogWindowSource
 {
-    private int m_iQueueLength;
+    private int queueLength;
     
-    private ArrayList<LogEntry> m_messages;
-    private final ArrayList<LogChangeListener> m_listeners;
-    private volatile LogChangeListener[] m_activeListeners;
-    
-    public LogWindowSource(int iQueueLength) 
-    {
-        m_iQueueLength = iQueueLength;
-        m_messages = new ArrayList<LogEntry>(iQueueLength);
-        m_listeners = new ArrayList<LogChangeListener>();
+    private ArrayList<LogEntry> messages;
+    private final ArrayList<LogChangeListener> listeners;
+    private volatile LogChangeListener[] activeListeners;
+
+    public LogWindowSource(int iQueueLength) {
+        queueLength = iQueueLength;
+        messages = new ArrayList<LogEntry>(iQueueLength);
+        listeners = new ArrayList<LogChangeListener>();
     }
-    
-    public void registerListener(LogChangeListener listener)
-    {
-        synchronized(m_listeners)
-        {
-            m_listeners.add(listener);
-            m_activeListeners = null;
+
+    public void registerListener(LogChangeListener listener) {
+        synchronized (listeners) {
+            listeners.add(listener);
+            activeListeners = null;
         }
     }
-    
-    public void unregisterListener(LogChangeListener listener)
-    {
-        synchronized(m_listeners)
-        {
-            m_listeners.remove(listener);
-            m_activeListeners = null;
+
+    public void unregisterListener(LogChangeListener listener) {
+        synchronized (listeners) {
+            listeners.remove(listener);
+            activeListeners = null;
         }
     }
-    
-    public void append(LogLevel logLevel, String strMessage)
-    {
+
+    public void append(LogLevel logLevel, String strMessage) {
         LogEntry entry = new LogEntry(logLevel, strMessage);
-        m_messages.add(entry);
-        LogChangeListener [] activeListeners = m_activeListeners;
-        if (activeListeners == null)
-        {
-            synchronized (m_listeners)
-            {
-                if (m_activeListeners == null)
-                {
-                    activeListeners = m_listeners.toArray(new LogChangeListener [0]);
-                    m_activeListeners = activeListeners;
+        messages.add(entry);
+        LogChangeListener[] activeListeners = this.activeListeners;
+        if (activeListeners == null) {
+            synchronized (listeners) {
+                if (this.activeListeners == null) {
+                    activeListeners = listeners.toArray(new LogChangeListener[0]);
+                    this.activeListeners = activeListeners;
                 }
             }
         }
-        for (LogChangeListener listener : activeListeners)
-        {
+        for (LogChangeListener listener : activeListeners) {
             listener.onLogChanged();
         }
     }
-    
-    public int size()
-    {
-        return m_messages.size();
+
+    public int size() {
+        return messages.size();
     }
 
-    public Iterable<LogEntry> range(int startFrom, int count)
-    {
-        if (startFrom < 0 || startFrom >= m_messages.size())
-        {
+    public Iterable<LogEntry> range(int startFrom, int count) {
+        if (startFrom < 0 || startFrom >= messages.size()) {
             return Collections.emptyList();
         }
-        int indexTo = Math.min(startFrom + count, m_messages.size());
-        return m_messages.subList(startFrom, indexTo);
+        int indexTo = Math.min(startFrom + count, messages.size());
+        return messages.subList(startFrom, indexTo);
     }
 
-    public Iterable<LogEntry> all()
-    {
-        return m_messages;
+    public Iterable<LogEntry> all() {
+        return messages;
     }
 }
